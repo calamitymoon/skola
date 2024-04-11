5e1d14ebe491969f03cb3b0c4003e2fb7c474880
9hb <domcalegit@seznam.cz>
2024-04-11 09:29:11 +0200

    pridavam projekt "prohlizec-obrazku" do predmetu pvp

---
+++ pvp/qt/prohlizec-obrazku/mainwindow.cpp
@@ -0,0 +1,57 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QImageReader>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionOtevrit_triggered()
+{
+    QString nazevSouboru = QFileDialog::getOpenFileName(this,
+                                                    tr("Otevřít obrázek"),
+                                                    "",
+                                                    tr("Image Files (*.png *.jpg *.jpeg)"));
+    if (!nazevSouboru.isEmpty()) {
+        QImageReader reader(nazevSouboru);
+        if (!reader.canRead()) {
+            QMessageBox::warning(this, tr("Chybička"), tr("Nelze načíst soubor"));
+            return;
+        }
+
+        QPixmap pixmap(nazevSouboru);
+        if (pixmap.isNull()) {
+            QMessageBox::warning(this, tr("Chybička"), tr("Nelze zobrazit obrázek"));
+            return;
+        }
+
+        zobrazitObrazek(pixmap);
+    }
+}
+
+void MainWindow::zobrazitObrazek(const QPixmap &pixmap)
+{
+    if (!obrazekDialog) {
+        obrazekDialog = new QDialog(this);
+        obrazekDialog->setWindowTitle(tr("Vybraný obrazek"));
+        QVBoxLayout *layout = new QVBoxLayout(obrazekDialog);
+        obrazekLabel = new QLabel;
+        layout->addWidget(obrazekLabel);
+        obrazekDialog->setLayout(layout);
+    }
+
+    obrazekLabel->setPixmap(pixmap);
+    obrazekDialog->show();
+}
---
+++ pvp/qt/prohlizec-obrazku/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QAction>
+#include <QDialog>
+#include <QLabel>
+#include <QVBoxLayout>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionOtevrit_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    void zobrazitObrazek(const QPixmap &pixmap);
+    QDialog *obrazekDialog = nullptr;
+    QLabel *obrazekLabel = nullptr;
+};
+#endif // MAINWINDOW_H

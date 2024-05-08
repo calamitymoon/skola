86910befd64d899d159387a0afb3bf6e38b02845
9hb <domcalegit@seznam.cz>
2024-05-08 22:56:20 +0200

    pridana nova kategorie "winforms" a do ni pridan projekt \'internet panic button\'

---
+++ pvp/winforms/internet panic button/IPB/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace IPB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.statusbar = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startButton.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startButton.ForeColor = System.Drawing.Color.Lime;
+            this.startButton.Location = new System.Drawing.Point(12, 64);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(80, 55);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.stopButton.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.ForeColor = System.Drawing.Color.Red;
+            this.stopButton.Location = new System.Drawing.Point(142, 64);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(80, 55);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // statusbar
+            // 
+            this.statusbar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusbar.Font = new System.Drawing.Font("DejaVu Sans Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.statusbar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.statusbar.Location = new System.Drawing.Point(15, 9);
+            this.statusbar.Name = "statusbar";
+            this.statusbar.Size = new System.Drawing.Size(204, 28);
+            this.statusbar.TabIndex = 1;
+            this.statusbar.Text = "Vypnuto";
+            this.statusbar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::IPB.Properties.Resources.close;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(213, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(15, 15);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(0, 0);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(234, 131);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.statusbar);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label statusbar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pvp/winforms/internet panic button/IPB/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPB
+{
+    public partial class Form1 : Form
+    {
+        #region rectangle style
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+        private const int HTBOTTOMRIGHT = 17;
+
+        private const int BORDER_WIDTH = 10;
+        private const int CORNER_RADIUS = 10;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddArc(new Rectangle(0, 0, CORNER_RADIUS * 2, CORNER_RADIUS * 2), 180, 90);
+                path.AddArc(new Rectangle(this.Width - CORNER_RADIUS * 2, 0, CORNER_RADIUS * 2, CORNER_RADIUS * 2), 270, 90);
+                path.AddArc(new Rectangle(this.Width - CORNER_RADIUS * 2, this.Height - CORNER_RADIUS * 2, CORNER_RADIUS * 2, CORNER_RADIUS * 2), 0, 90);
+                path.AddArc(new Rectangle(0, this.Height - CORNER_RADIUS * 2, CORNER_RADIUS * 2, CORNER_RADIUS * 2), 90, 90);
+                path.CloseAllFigures();
+
+                this.Region = new Region(path);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT)
+            {
+                m.Result = (IntPtr)HTCAPTION;
+            }
+        }
+
+        private void RoundedForm_Load(object sender, EventArgs e)
+        {
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, this.Width, this.Height, CORNER_RADIUS, CORNER_RADIUS));
+        }
+
+        #endregion rectangle style
+
+        private bool blokovat = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.BackColor = Color.FromArgb(30, 30, 30);
+        }
+
+        #region buttons
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            blokovat = true;
+            vypnout();
+            upravitStatusbar();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            blokovat = false;
+            zapnout();
+            upravitStatusbar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion buttons
+
+        #region functions
+
+        private void upravitStatusbar()
+        {
+            statusbar.Text = blokovat ? "Zapnuto" : "Vypnuto";
+        }
+        private void zapnout()
+        {
+            string prikaz = "netsh advfirewall firewall delete rule name=\"block all outbound traffic\"";
+
+            ProcessStartInfo pInfo = new ProcessStartInfo("cmd.exe", "/c " + prikaz);
+            {
+                pInfo.CreateNoWindow = true;
+                pInfo.UseShellExecute = false;
+            }
+
+            using (Process process = Process.Start(pInfo))
+            {
+                process.WaitForExit();
+            }
+        }
+        private void vypnout()
+        {
+            string prikaz = "netsh advfirewall firewall add rule name=\"block all outbound traffic\" dir=out action=block";
+
+            ProcessStartInfo pInfo = new ProcessStartInfo("cmd.exe", "/c " + prikaz);
+            {
+                pInfo.CreateNoWindow = true;
+                pInfo.UseShellExecute = false;
+            }
+
+            using (Process process = Process.Start(pInfo))
+            {
+                process.WaitForExit();
+            }
+
+            #endregion buttons
+
+        }
+    }
+}

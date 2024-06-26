49d14c72d57e5902650344cbfc7973555145369c
9hb <domcalegit@seznam.cz>
2024-06-26 20:57:06 +0200

    presun na novy notebook

---
+++ pvp/winforms/textova analyza/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace textova_analyza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bNacist = new System.Windows.Forms.Button();
+            this.tbObsah = new System.Windows.Forms.TextBox();
+            this.bSpustit = new System.Windows.Forms.Button();
+            this.lStatistika = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bNacist
+            // 
+            this.bNacist.Location = new System.Drawing.Point(385, 12);
+            this.bNacist.Name = "bNacist";
+            this.bNacist.Size = new System.Drawing.Size(100, 23);
+            this.bNacist.TabIndex = 0;
+            this.bNacist.Text = "Načíst soubor";
+            this.bNacist.UseVisualStyleBackColor = true;
+            this.bNacist.Click += new System.EventHandler(this.bNacist_Click);
+            // 
+            // tbObsah
+            // 
+            this.tbObsah.Location = new System.Drawing.Point(12, 28);
+            this.tbObsah.Multiline = true;
+            this.tbObsah.Name = "tbObsah";
+            this.tbObsah.ReadOnly = true;
+            this.tbObsah.Size = new System.Drawing.Size(367, 504);
+            this.tbObsah.TabIndex = 2;
+            this.tbObsah.Text = "[ zobrazení obsah souboru ]";
+            this.tbObsah.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bSpustit
+            // 
+            this.bSpustit.Location = new System.Drawing.Point(410, 41);
+            this.bSpustit.Name = "bSpustit";
+            this.bSpustit.Size = new System.Drawing.Size(75, 23);
+            this.bSpustit.TabIndex = 1;
+            this.bSpustit.Text = "Analyzovat";
+            this.bSpustit.UseVisualStyleBackColor = true;
+            this.bSpustit.Click += new System.EventHandler(this.bSpustit_Click);
+            // 
+            // lStatistika
+            // 
+            this.lStatistika.AutoSize = true;
+            this.lStatistika.Location = new System.Drawing.Point(12, 12);
+            this.lStatistika.Name = "lStatistika";
+            this.lStatistika.Size = new System.Drawing.Size(139, 13);
+            this.lStatistika.TabIndex = 3;
+            this.lStatistika.Text = "[ počet slov, vět, odstavců ]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 544);
+            this.Controls.Add(this.lStatistika);
+            this.Controls.Add(this.bSpustit);
+            this.Controls.Add(this.tbObsah);
+            this.Controls.Add(this.bNacist);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Analýza textu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bNacist;
+        private System.Windows.Forms.TextBox tbObsah;
+        private System.Windows.Forms.Button bSpustit;
+        private System.Windows.Forms.Label lStatistika;
+    }
+}
+
---
+++ pvp/winforms/textova analyza/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace textova_analyza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Počítání slov, vět a odstavců
+
+        private int zpocitatSlova(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return 0;
+            return text.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        private int zpocitatVety(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return 0;
+            return Regex.Matches(text, @"[.!?]").Count;
+        }
+
+        private int zpocitatOdstavce(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return 0;
+            return text.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).Count();
+        }
+
+        #endregion Počítání slov, vět a odstavců
+
+        #region Načítání souborů a analýza
+
+        // Načíst soubor k analýze
+        private void bNacist_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Textové soubory|*.txt|Všechny soubory|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                tbObsah.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        // Spustit analýzu
+        private void bSpustit_Click(object sender, EventArgs e)
+        {
+            string obsah = tbObsah.Text;
+
+            int pocetSlov = zpocitatSlova(obsah);
+            int pocetVet = zpocitatVety(obsah);
+            int pocetOdstavcu = zpocitatOdstavce(obsah);
+
+            lStatistika.Text = $"Slova: {pocetSlov}, věty: {pocetVet}, odstavce: {pocetOdstavcu}";
+        }
+        #endregion Načítání souborů a analýza
+    }
+}

0a439faa69b47ab1274d6f5109bf82319de9afb8
9hb <domcalegit@seznam.cz>
2024-09-06 11:52:54 +0200

    zkouska popupu v html
    pouzito html, css a js

---
+++ pvp/html/popup-window/assets/script.js
@@ -0,0 +1,9 @@
+function zavrit() {
+    document.getElementById("jedna").style.display = "none";
+    document.getElementById("dva").style.display = "initial";
+}
+
+function otevrit() {
+    document.getElementById("jedna").style.display = "initial";
+    document.getElementById("dva").style.display = "none";
+}

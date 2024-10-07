4ea4fe1bd7791327b0a21bc826242b5d2fb28fae
9hb <domcalegit@seznam.cz>
2024-10-07 09:27:46 +0200

    Pridano odhlasovani a upravovani uzivatelu

--- pvp/html a php/prvni-s-f3/app/controllers/User.php
+++ pvp/html a php/prvni-s-f3/app/controllers/User.php
@@ -49,4 +49,38 @@ class User
         }
     }
 
+    public function getOdhlasit(\Base $base)
+    {
+        $base->clear('SESSION.id');
+        $base->reroute('/pokus');
+    }
+
+    public function getSmazat(\Base $base)
+    {
+        $id = $base->get('GET.id');
+        $user = new \models\User();
+        $user->load($id);
+        $user->delete();
+        $base->reroute('/pokus');
+    }
+
+    public function postUpravit(\Base $base)
+    {
+        $uzivatel = new \models\User();
+        $uz = $uzivatel->findone($base->get('PARAMS.id'));
+        $uz->copyFrom('POST');
+        $uz->save();
+        $base->reroute('/user/list');
+    }
+
+    public function getUpravit(\Base $base)
+    {
+        $user = new \models\User();
+        $uz = $user->findone($base->get('PARAMS.id'));
+        $base->set('user', $uz);
+        $base->set('title', 'Upravit uzivatele');
+        $base->set('content', '/user/upravit.html');
+        echo \Template::instance()->render('index.html');
+    }
+
 }

80c2ade1415c66c93b7dca8b5f7e5d19070e7468
9hb <domcalegit@seznam.cz>
2024-11-05 10:55:43 +0100

    vymazana cast s vykreslovanim segmentu (do jineho souboru)

--- elm/4. rocnik/arduino/oled-display/oled-display.ino
+++ elm/4. rocnik/arduino/oled-display/oled-display.ino
@@ -78,63 +78,6 @@ void blinkingText() {
   delay(250);
 }
 
-const byte segmentDisplay[] = {
-    0b11000000, // '0'
-    0b11111001, // '1'
-    0b10100100, // '2'
-    0b10110000, // '3'
-    0b10011001, // '4'
-    0b10010010, // '5'
-    0b10000010, // '6'
-    0b11111000, // '7'
-    0b10000000, // '8'
-    0b10010000, // '9'
-    0b10001000, // 'A'
-    0b10000011, // 'B'
-    0b11000110, // 'C'
-    0b10100001, // 'D'
-    0b10000110, // 'E'
-    0b10001110  // 'F'
-};
-
-void vykreslitZnak(char znak) {
-  byte segmenty;
-
-  // kontrola jaky znak je predanej funkci (podle toho vybira segmenty z arraye segmentDisplay)
-  if (znak >= '0' && znak <= '9') {
-    // indexovani pomoci ascii hodnoty
-    segmenty = segmentDisplay[znak - '0'];
-  } else if (znak >= 'A' && znak <= 'F') {
-    segmenty = segmentDisplay[znak - 'A' + 10];
-  } else { return; }
-
-  vykreslitSegment(segmenty & 0b01000000, 10, 0, 30, 5);  // A
-  vykreslitSegment(segmenty & 0b00100000, 40, 5, 5, 30);  // B
-  vykreslitSegment(segmenty & 0b00010000, 40, 40, 5, 30); // C
-  vykreslitSegment(segmenty & 0b00001000, 10, 70, 30, 5); // D
-  vykreslitSegment(segmenty & 0b00000100, 5, 40, 5, 30);  // E
-  vykreslitSegment(segmenty & 0b00000010, 5, 5, 5, 30);   // F
-  vykreslitSegment(segmenty & 0b00000001, 10, 35, 30, 5); // G
-}
-
-void vykreslitSegment(bool stav, int x, int y, int sirka, int vyska) {
-  if (stav) {
-    display.fillRect(x, y, sirka, vyska, SSD1306_WHITE);
-  }
-}
-
-// void segDisplay() {
-//   for (int i = 0; i < 16; i++) {
-//     display.clearDisplay();
-//     display.setTextColor(WHITE);
-//     display.setCursor(0, 10);
-//     display.print("segmenty: ");
-//     display.print(segmentDisplay[i], BIN);  // binarni cislo
-//     display.display();
-//     delay(1000);
-//   }
-// }
-
 void setup() {
   Serial.begin(9600);
 
@@ -159,5 +102,4 @@ void loop() {
   // delay(250);
 
   // segDisplay();
-  vykreslitZnak("A");
 }

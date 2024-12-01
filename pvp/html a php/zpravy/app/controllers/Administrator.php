c45540df56a95f8b0102cdff7859f9f8849e9a74
9hb <domcalegit@seznam.cz>
2024-12-01 21:52:22 +0100

    vytvoren backend pro spravu roli a spravu uzivatelu

--- pvp/html a php/zpravy/app/controllers/Administrator.php
+++ pvp/html a php/zpravy/app/controllers/Administrator.php
@@ -4,14 +4,25 @@ namespace controllers;
 
 class Administrator extends AbstractController
 {
-    public function getPanel(\Base $base)
+    public function getSpravaRoli(\Base $base)
     {
         $base->set('title', 'Admin panel');
         $role = new \models\Role(); // vyrobit model uzivatelu
         $users = new \models\Uzivatel(); // vyrobit model uzivatelu
         $base->set('role', $role->find());
         $base->set('users', $users->find());
-        $base->set('content', '/administrator/panel.html');
+        $base->set('content', '/administrator/spravaroli.html');
+        echo \Template::instance()->render('index.html');
+    }
+    
+    public function getSpravaUzivatelu(\Base $base)
+    {
+        $base->set('title', 'Admin panel');
+        $role = new \models\Role(); // vyrobit model uzivatelu
+        $users = new \models\Uzivatel(); // vyrobit model uzivatelu
+        $base->set('role', $role->find());
+        $base->set('users', $users->find());
+        $base->set('content', '/administrator/spravauzivatelu.html');
         echo \Template::instance()->render('index.html');
     }
 }

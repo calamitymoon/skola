dbcd98aea5029b525669d9b0ebca581bd5f888fc
9hb <domcalegit@seznam.cz>
2024-12-18 13:03:14 +0100

    pridano generovani pdfka profilu uzivatele

--- pvp/html a php/zpravy/app/controllers/PDF.php
+++ pvp/html a php/zpravy/app/controllers/PDF.php
@@ -4,6 +4,15 @@ namespace controllers;
 
 class PDF extends AbstractController
 {
+    public function strankaPDFprofilUzivatele(\Base $base)
+    {
+        $uzivatele = new \models\Uzivatel();
+        $base->set('uzivatele', $uzivatele->find());
+        $base->set('title', 'Profily uživatelů do PDF');
+        $base->set('content', '/pdf/generace/profil.html');
+        echo \Template::instance()->render('index.html');
+    }
+
     public function vytvorPDF(\Base $base)
     {
         $base->set('title', 'PDF soubor');
@@ -20,5 +29,25 @@ class PDF extends AbstractController
         header('Content-Type: application/pdf');
         echo base64_encode($pdf->stream("uzivatele.pdf", array("Attachment" => 0)));
         // $pdf->stream(); //   direct link je stazeni souboru
+    }
+
+    public function vytvorPDFprofilUzivatele(\Base $base)
+    {
+        $base->set('title', 'Profil uživatele do PDF');
+        $uzivatele = new \models\Uzivatel();
+        $idcko = $base->get('PARAMS.id');
+        $vybranyUzivatel = $uzivatele->findone(['id = ?', $idcko]);
+
+        $base->set('vybranyUzivatel', $vybranyUzivatel);
+        $base->set('title', 'Profil uživatele do PDF');
+        $pdf = new \Dompdf\Dompdf();
+        $pdf->setPaper('A4', 'portrait');
+        $pdf->loadHtml(\Template::instance()->render('/pdf/profil.html'));
+        $pdf->render();
+        
+        file_put_contents($base->get('UPLOADS') . 'pdf/uzivatele' , $pdf->output());
+
+        header('Content-Type: application/pdf');
+        echo base64_encode($pdf->stream("profil.pdf", array("Attachment" => 0)));
     }
 }

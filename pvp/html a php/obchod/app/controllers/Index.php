401c12ee8c180c3cf7b64fee98a4bfa771beda2c
9hb <domcalegit@seznam.cz>
2025-02-05 13:10:36 +0100

    pridano vytvareni obchodu a pobocek + v sekci debug je full dump vsech dat

--- pvp/html a php/obchod/app/controllers/Index.php
+++ pvp/html a php/obchod/app/controllers/Index.php
@@ -44,22 +44,164 @@ class Index
         $base->set('title', 'Admin panel');
         $base->set('content', '/admin/panel.html');
 
-        $typy = new \models\Typ();
-        $typy = json_encode($typy->find());
-        $base->set('typy', $typy);
+        
+        // $typy = new \models\Typ();
+        // $typy = $typy->find();
+        // $results = [];
+        // foreach ($typy as $typ) {
+        //     $results[] = $typ->cast();
+        // }
+        // $base->set('typy', json_encode($results));
+        $typ = new \models\Typ();
+        $typyList = $typ->find();
 
+        $nazvy = [];
+        $ids = [];
+
+        foreach ($typyList as $t) {
+            $nazvy[] = $t->typy;
+            $ids[] = $t->id;
+        }
+
+        $typyResults = [];
+        foreach ($typyList as $typ) {
+            $typyResults[] = $typ->cast();
+        }
+        $base->set('typy', json_encode($typyResults));
+        $base->set('typy_nazvy', json_encode($nazvy));
+        $base->set('typy_ids', json_encode($ids));
+
+
+        // $pobocka = new \models\Pobocka();
+        // $pobockyList = $pobocka->find();
+        // $results = [];
+        // foreach ($pobockyList as $pob) {
+        //     $results[] = $pob->cast();
+        // }
+        // $base->set('pobocka', json_encode($results));
         $pobocka = new \models\Pobocka();
-        $pobocka = json_encode($pobocka->find());
-        $base->set('pobocka', $pobocka);
+        $pobockyList = $pobocka->find();
+
+        $nazvy = [];
+        $adresy = [];
+
+        foreach ($pobockyList as $pob) {
+            $nazvy[] = $pob->nazev;
+            $adresy[] = $pob->adresa;
+        }
+
+        $pobockaResults = [];
+        foreach ($pobockyList as $pob) {
+            $pobockaResults[] = $pob->cast();
+        }
+        $base->set('pobocka', json_encode($pobockaResults));
+        $base->set('pobocka_nazvy', json_encode($nazvy));
+        $base->set('pobocka_adresy', json_encode($adresy));
+
+
+        // $obchod = new \models\Obchod();
+        // $obchodyList = $obchod->find();
+        // $results = [];
+        // foreach ($obchodyList as $ob) {
+        //     $results[] = $ob->cast();
+        // }
+        // $base->set('obchod', json_encode($results));
+        $obchod = new \models\Obchod();
+        $obchodyList = $obchod->find();
+
+        $typy = [];
+        $kategorie = [];
+
+        foreach ($obchodyList as $ob) {
+            $typy[] = $ob->typ;
+            $kategorie[] = $ob->kategorie;
+        }
+
+        $obchodResults = [];
+        foreach ($obchodyList as $ob) {
+            $obchodResults[] = $ob->cast();
+        }
+        $base->set('obchod', json_encode($obchodResults));
+        $base->set('obchod_typy', json_encode($typy));
+        $base->set('obchod_kategorie', json_encode($kategorie));
+
+
+        // $kategorie = new \models\Kategorie();
+        // $kategorieList = $kategorie->find();
+        // $results = [];
+        // foreach ($kategorieList as $kat) {
+        //     $results[] = $kat->cast();
+        // }
+        // $base->set('kategorie', json_encode($results));
+        $kategorie = new \models\Kategorie();
+        $kategorieList = $kategorie->find();
+
+        $nazvy = [];
+        $ids = [];
+
+        foreach ($kategorieList as $kat) {
+        $nazvy[] = $kat->vsechny;
+        $ids[] = $kat->_id;
+        }
+
+        $kategorieResults = [];
+        foreach ($kategorieList as $kat) {
+            $kategorieResults[] = $kat->cast();
+        }
+        $base->set('kategorie', json_encode($kategorieResults));
+        $base->set('kategorie_nazvy', json_encode($nazvy));
+        $base->set('kategorie_ids', json_encode($ids));
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
         $obchod = new \models\Obchod();
-        $obchod = json_encode($obchod->find());
-        $base->set('obchod', $obchod);
+        $pobocka = new \models\Pobocka();
+        $typ = new \models\Typ();
 
-        $kategorie = new \models\Kategorie();
-        $kategorie = json_encode($kategorie->find());
-        $base->set('kategorie', $kategorie);
+        $obchodyList = $obchod->find();
+        $obchodL = [];
+        foreach ($obchodyList as $obchod) {
+            $obchodL[] = $obchod->cast();
+        }
+        $base->set('obchodL', $obchodL);
 
+        $pobockyList = $pobocka->find();
+        $pobockaL = [];
+        foreach ($pobockyList as $pobocka) {
+            $pobockaL[] = $pobocka->cast();
+        }
+        $base->set('pobockaL', $pobockaL);
+
+        $typyList = $typ->find();
+        $typyL = [];
+        foreach ($typyList as $typ) {
+            $typyL[] = $typ->cast();
+        }
+        $base->set('typyL', $typyL);
+        
         echo \Template::instance()->render('index.html');
+    }
+
+    public function postAdminPanel (\Base $base)
+    {
+        foreach ($f3->POST as $key => $value) {
+            if (strpos($key, 'typ_') === 0) {
+                $obchodId = substr($key, 4);
+                $obchod = new \models\Obchod();
+                $obchod->load(['id=?', $obchodId]);
+                $obchod->typ = $value;
+                $obchod->save();
+            }
+            
+            if (strpos($key, 'pobocky_') === 0) {
+                $obchodId = substr($key, 8, -2);
+                $obchod = new \models\Obchod();
+                $obchod->load(['id=?', $obchodId]);
+                $obchod->pobocky = $value;
+                $obchod->save();
+            }
+        }
+
+        $f3->reroute('/admin');
     }
 }

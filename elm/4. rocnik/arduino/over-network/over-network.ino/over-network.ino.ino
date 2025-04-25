9128fcc03b252ce9e097200eab2065f1990fd7c2
9hb <domcalegit@seznam.cz>
2025-04-25 15:32:22 +0200

    uprava arduino scriptu - co2ntroler

--- elm/4. rocnik/arduino/over-network/over-network.ino/over-network.ino.ino
+++ elm/4. rocnik/arduino/over-network/over-network.ino/over-network.ino.ino
@@ -1,28 +1,40 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <ArduinoJson.h>
+#include <SD.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress server(192, 168, 129, 43);
+IPAddress ip(192, 168, 1, 177);              // Statická IP (měň dle své sítě)
+IPAddress gateway(192, 168, 1, 1);           // Gateway
+IPAddress subnet(255, 255, 255, 0);          // Maska podsítě
+IPAddress server(192, 168, 129, 35);         // Server IP (měň dle potřeby)
 int port = 7878;
+
 EthernetClient client;
 
 float temperature = 22.5;
 float humidity = 45.2;
 float pressure = 1013.25;
 int co2 = 420;
+int mistnost = 0;
 
 void setup() {
     Serial.begin(9600);
     delay(1000);
     Serial.println("zacinam...");
-    
-    // if (Ethernet.begin(mac) == 0) {
-    //     Serial.println("Failed to configure Ethernet using DHCP");
-    //     IPAddress ip(10, 12, 69, 51);
-    //     Ethernet.begin(mac, ip);
-    // }
-    
+
+    // pokus o dhcp
+    if (Ethernet.begin(mac) == 0) {
+        Serial.println("DHCP selhalo, nastavuji statickou IP");
+        Ethernet.begin(mac, ip, gateway, subnet);
+    }
+
+    // pokus o nacteni sd karty
+    if (!SD.begin(4)) {
+        Serial.println("sd karta << nenactena");
+        while (1);
+    }
+    Serial.println("sd karta >> nactena");
+
     delay(1000);
     Serial.print("IP adresa Arduina: ");
     Serial.println(Ethernet.localIP());
@@ -33,39 +45,39 @@ void loop() {
     humidity = random(30, 70);              // 30 - 70
     pressure = random(980, 1050);           // 980 - 1050
     co2 = random(350, 450);                 // 350 - 450
-    
+    mistnost = random(1, 10);               // 1 - 10
+
     odeslatNaServer();
     delay(10000);
 }
 
 void odeslatNaServer() {
-    Serial.println("pripojovani...");
-    
+    String dataServer   = String(mistnost) + "," + millis() + "," + temperature + "," + humidity + "," + pressure + "," + co2 + "," + "senzor";
+    String dataPamet    = String(mistnost) + "," + millis() + "," + temperature + "," + humidity + "," + pressure + "," + co2;
+
     if (client.connect(server, port)) {
-        Serial.println("pripojeni se povedlo");
-        
-        StaticJsonDocument<256> json;
-        
-        json["odkud"] = "Místnost 1";
-        json["timestamp"] = String(millis());
-        json["teplota"] = String(temperature, 1);
-        json["vlhkost"] = String(humidity);
-        json["tlak"] = String(pressure);
-        json["co2"] = String(co2);
-        
-        String jsonString;
-        serializeJson(json, jsonString);
-        
-        jsonString += "\n";
-        
-        client.print(jsonString);
-        
+        Serial.println("[+] pripojeni");
+
+        client.print(dataServer);
+
         Serial.print("odeslano: ");
-        Serial.print(jsonString);
-        
+        Serial.print(dataServer);
+
         client.stop();
-        Serial.println("pripojeni uzavreno");
     } else {
-        Serial.println("pripojeni selhalo");
+        Serial.println("[-] pripojeni");
+
+        ulozitOffline(dataPamet);
+    }
+}
+
+void ulozitOffline(String dataPamet) {
+    File soubor = SD.open("offline.txt", FILE_WRITE);
+    if (soubor) {
+        soubor.println(dataPamet);
+        soubor.close();
+        Serial.println("[+] offline");
+    } else {
+        Serial.println("nemuzu ulozit data do offline.txt");
     }
 }
